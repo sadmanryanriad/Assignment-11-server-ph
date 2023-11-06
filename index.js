f463eab4b972c61e61a975ebe1154bb1df3d94b7
sadmanryanriad <sadmanryanriad@gmail.com>
2023-11-06 22:40:52 +0600

    error fixed

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors({
-    origin: [
-        'http://localhost:5173',
-    ],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ni8nft9.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,41 +25,45 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-
+    // MongoDB collections
     const rooms = client.db("Assignment-11").collection("rooms");
-
+    const booked = client.db("Assignment-11").collection("booked");
 
     //get rooms
     app.get("/rooms", async (req, res) => {
-        const result = await rooms.find().toArray();
-        res.send(result);
-      });
+      const result = await rooms.find().toArray();
+      res.send(result);
+    });
 
     //find room with id
-        app.get("/rooms/:id", async (req, res) => {
-            const id = req.params.id;   
-            const query = { _id: new ObjectId(id) };
-            const result = await rooms.find(query).toArray();
-            res.send(result);
-          });
+    app.get("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      try {
+        const query = { _id: new ObjectId(id) };
+        const result = await rooms.findOne(query);
+
+        if (result) {
+          res.send(result);
+        }
+      } catch (error) {
+        res.status(400).send("Invalid room ID");
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
