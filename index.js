101dd04aac0bf488cba6f40fbcba4e80179c441c
sadmanryanriad <sadmanryanriad@gmail.com>
2023-11-08 18:55:39 +0600

    added https://assignment-11-785f0.web.app/ link to cors origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://assignment-11-785f0.web.app/"],
     credentials: true,
   })
 );
